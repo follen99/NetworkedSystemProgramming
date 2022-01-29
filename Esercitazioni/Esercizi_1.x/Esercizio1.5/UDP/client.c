11115129fa372ed33a83602ad7f0b2ce8d0fc9e9
follen99 <giulianoranauroiphone1@gmail.com>
2022-01-29 19:38:32 +0100

    es 1.5 UDP

---
+++ Esercitazioni/Esercizi_1.x/Esercizio1.5/UDP/client.c
@@ -0,0 +1,50 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+#define PROTOPORT  5193
+#define LOCALHOST  "127.0.0.1"  
+
+int main(int argc, char *argv[]){
+    char buf[256];
+    int clientSocket;
+    struct sockaddr_in remoteAddr;
+    socklen_t remoteAddrLen;
+
+    char *host;
+    if(argc > 1){
+        host = argv[1];
+    }else{
+        host = LOCALHOST;
+    }
+
+    clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+    memset(&remoteAddr, 0, sizeof(remoteAddr)); // pulisco l'indirizzo
+
+    remoteAddr.sin_family = AF_INET;
+    remoteAddr.sin_port = htons(PROTOPORT);
+    remoteAddr.sin_addr.s_addr = inet_addr(host);   // setto l'host
+    remoteAddrLen = sizeof(remoteAddr);
+
+    memset(&buf, 0, sizeof(buf));   // pulisco il buf
+
+    // fase di invio
+
+    printf("Inserisci il tuo nome (max 255 char): "); scanf("%s", &buf);
+
+    sendto(clientSocket, buf, sizeof(buf), 0, (struct sockaddr*) &remoteAddr, remoteAddrLen); // il numero di byte che voglaimo invare corrisponde a 2*sizeof(int)
+
+    memset(buf, 0, sizeof(buf));
+
+    recvfrom(clientSocket, buf, sizeof(buf), 0, (struct sockaddr*) &remoteAddr, &remoteAddrLen);
+
+    printf("%s", buf);    //stampo il contenuto del buffer
+    close(clientSocket);
+    return 0;
+}
---
+++ Esercitazioni/Esercizi_1.x/Esercizio1.5/UDP/server.c
@@ -0,0 +1,54 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PROTOPORT 5193
+#define BUFFER_SIZE 256
+int main(int argc, char const *argv[])
+{
+    char buf[BUFFER_SIZE];
+    int serverSocket;
+    socklen_t clientAddrLen;
+
+    int visits = 0;
+
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddr;
+
+    serverSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PROTOPORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bind(serverSocket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));
+    clientAddrLen = sizeof(clientAddr);
+
+    
+    while(1){
+        // fase di ricezione
+
+        char name[256];
+        memset(name, 0, sizeof(name));
+
+        recvfrom(serverSocket, name, sizeof(name), 0, (struct sockaddr*) &clientAddr, &clientAddrLen);
+        visits++;
+
+        memset(buf, 0, sizeof(buf));
+        //sprintf(buf, "Questo server è stato visitato %ld volte", prod);
+        sprintf(buf, "%s, Sei il %d° utente del server!\n", name, visits);
+
+        
+
+
+
+        sendto(serverSocket, buf, sizeof(buf), 0, (struct sockaddr*) &clientAddr, clientAddrLen);
+    }
+
+
+
+    return 0;
+}
